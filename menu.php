5b882d0e96a2d7450e7a7a5ac736c710abe70ea9
SanderLuuk <sander.luuk@khk.ee>
2017-01-18 10:47:10 +0200

    create menu objects

--- menu.php
+++ menu.php
@@ -8,4 +8,5 @@
 // menu.php - create page menu
 // create menu template objects
 //for menu and menu items
-$menu =new template('menu.menu');
+$menu =new template('menu.menu'); //in menu directory is file menu.html menu/menu.html
+$item =new template('menu.item');
