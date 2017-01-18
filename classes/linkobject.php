a718011458efff46fa0fc9618f5bdfa8c6c28b17
SanderLuuk <sander.luuk@khk.ee>
2017-01-18 09:15:33 +0200

    Create linkobject construct and base Url

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -11,6 +11,17 @@
 require_once 'http.php';
 //only for testing
 class linkobject extends http
-{
-
-}
+{//class begin
+    //class variables
+    var $baseUrl = false; //base url
+    var $protocol = 'http://'; //prtocpö for url - http
+    var $delim = '&amp'; //&html tag
+    var $eq = '='; // = for url element pair element_name = element value
+    //class methods
+    //onstruct
+    //create base url: http//XXX.XXX.XXXX.XXXX/path_to_file.php
+    function construct(){
+        parent::__construct();
+        $this->baseURL = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }//construct
+}//class end
