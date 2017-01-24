564dc794ace39258ea3447a84a6d94f1c927018d
SanderLuuk <sander.luuk@khk.ee>
2017-01-24 12:45:31 +0200

    create menu database query result

--- menu.php
+++ menu.php
@@ -6,41 +6,27 @@
  * Time: 10:34
  */
 // menu.php - create page menu
-// create menu template objects
+// create menu and item objects
 //for menu and menu items
-$menu =new template('menu.menu'); //in menu directory is file menu.html menu/menu.html
+$menu = new template('menu.menu'); //in menu directory is file menu.html menu/menu.html
 $item =new template('menu.item');
-//menu item creation - begin
-//add pairs  of item template element names and real values
-$item->set('name', 'Esimene leht');
-//make link element
-$link = $http->getLink(array('act'=>'first'));
-$item->set('link',$link);
-//control created item output
-//echo '<pre>';
-//print_r($item);
-//echo '</pre>';
-//add menu item top menu
-$menu->set('items',$item->parse()); //v2ljatrykki v6imaldab parse
-//menu item creation - begin
-//
-//menu item creation - begin
-//add pairs  of item template element names and real values
-$item->set('name', 'Teine leht');
-//make link element
-$link = $http->getLink(array('act'=>'second'));
-$item->set('link',$link);
-//control created item output
-//echo '<pre>';
-//print_r($item);
-//echo '</pre>';
-//add menu item top menu
-$menu->add('items',$item->parse()); // add another item to menu!
-//control created menu outputs
-/*echo '<pre>';
-print_r($menu);
-echo '</pre>';*/
-//output $menu->parse();
-//echo $menu->parse();
+//main meny content query
+$sql = 'SELECT content_id, title from content where '.
+    'parent_id = "0" and show_in_menu = "1"';
+$sql = $sql.'order by sort asc';
+//get menu data from database
+$res = $db ->getArray($sql);
+//create menu items from query result
+if($res != false){
+    foreach ( $res as $page){
+        //add content to menu item
+        $item->set('name',$page['title']);
+        $link = $http->getLink(array('page_id'=>$page ['content_id']));
+        $item->set('link', $link);
+        //add item to menu
+        $menu->add('items', $item->parse());
 
+    }
+}
+$tmpl->set('menu', $menu->parse());
 ?>
