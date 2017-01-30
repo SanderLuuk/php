9f0fd047bd1fcd7319e25a26b9b17958745ad619
SanderLuuk <sander.luuk@khk.ee>
2017-01-30 12:52:34 +0200

    filling the form in mainpage.

---
+++ acts/login.php
@@ -0,0 +1,16 @@
+<?php
+$form = new Template('login');
+
+$form-> set('error', $sess->get('login_error'));
+$sess->del('login_error');
+
+$form->set('submit', tr('Logi sisse'));
+$form->set('username_str', tr('Kasutajanimi'));
+$form->set('password_str', tr('Parool'));
+
+$form->set('username', $http->get('username', true));
+
+$link = $http->getLink(array('act' => 'login_do'));
+$form -> set('action', $link);
+$tmpl->set('content', $form->parse());
+?>
