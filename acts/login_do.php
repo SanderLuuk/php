e16ef8ef437accac27cd52de8c753c8611bb0d5d
SanderLuuk <sander.luuk@khk.ee>
2017-01-30 13:02:46 +0200

    filling the login from and connecting to db.

---
+++ acts/login_do.php
@@ -0,0 +1,19 @@
+<?php
+$username = $http->get('username');
+$password = $http->get('password');
+
+$sql = 'select * FROM user WHERE '.'username ='.fixDb($username).' AND '.'password='.fixDb(md5($password)).' AND '.'
+is_active=1';
+$res =$db->getArray($sql);
+
+if ($res === false){
+    $sess->set ('login_error', tr('Viga sisselogimisel'));
+    $link = $http->getLink(array('act'=>'login'), array('username'));
+    $http->redirect($link);
+}
+else{
+    $sess->createSession($res[0]);
+    //now we have to redirect to index.php
+}
+
+?>
